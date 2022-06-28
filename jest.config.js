8f3ee9d81e2af6e9acc70043d68cc4cecadb9cf3
jgeschwendt <joshuageschwendt@rockethomes.com>
2022-06-28 08:56:01 -0400

    update

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 const USE_BABEL = Boolean(process.env.USE_BABEL);
 
+/** @type {import('jest').Config} */
 module.exports = {
   collectCoverageFrom: ["src/**/*.ts", "!src/**/*.test.ts"],
+  coverageProvider: USE_BABEL ? 'babel' : 'v8',
   coverageThreshold: {
     "src/**/*.ts": {
       branches: 100,
